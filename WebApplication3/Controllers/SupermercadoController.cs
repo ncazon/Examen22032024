30eb8055f307bc5ec5bd5d10a6d19f19c6b43b83
nelson.cazon <nelson.cazon@NCAZON>
2024-03-22 22:07:52 -0400

    Agregar archivos de proyecto.

---
+++ WebApplication3/Controllers/SupermercadoController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+
+    public class SupermercadoController : Controller
+    {
+        private static List<Producto> productos = new List<Producto>
+    {
+        new Producto { Id = 1, Nombre = "Atún VanCamps", Precio = 12, Imagen = "atun.jpg" },
+        new Producto { Id = 2, Nombre = "Queso menonita", Precio = 45, Imagen = "queso.jpg" }
+    };
+
+        private static List<Elemento> carrito = new List<Elemento>();
+
+        public IActionResult Index()
+        {
+            return View(productos);
+        }
+
+        public IActionResult AgregarAlCarrito(int id)
+        {
+            var producto = productos.FirstOrDefault(p => p.Id == id);
+            if (producto != null)
+            {
+                var elemento = carrito.FirstOrDefault(e => e.Producto.Id == id);
+                if (elemento != null)
+                {
+                    elemento.Cantidad++;
+                }
+                else
+                {
+                    carrito.Add(new Elemento { Producto = producto, Cantidad = 1 });
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Carrito()
+        {
+            return View(carrito);
+        }
+
+        public IActionResult EliminarDelCarrito(int id)
+        {
+            var elemento = carrito.FirstOrDefault(e => e.Producto.Id == id);
+            if (elemento != null)
+            {
+                if (elemento.Cantidad > 1)
+                {
+                    elemento.Cantidad--;
+                }
+                else
+                {
+                    carrito.Remove(elemento);
+                }
+            }
+            return RedirectToAction("Carrito");
+        }
+
+        public IActionResult FinalizarCompra()
+        {
+            var montoTotal = carrito.Sum(e => e.Producto.Precio * e.Cantidad);
+            carrito.Clear();
+            ViewBag.MontoTotal = montoTotal;
+            return View("CompraRealizada");
+        }
+    }
+
+}
---
+++ WebApplication3/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication3.Models
+{
+  
+
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+        public string Imagen { get; set; }
+    }
+    public class Elemento
+    {
+        public Producto Producto { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
